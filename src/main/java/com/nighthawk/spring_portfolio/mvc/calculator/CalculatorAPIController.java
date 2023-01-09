9d4f5bc86607f50d779bfc9f35c90a8e2f5674f3
Armaan Shamsaasef <89277945+xXAASXx@users.noreply.github.com>
2023-01-08 21:22:11 -0800

    Create CalculatorAPIController.java

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/CalculatorAPIController.java
@@ -0,0 +1,27 @@
+package com.nighthawk.spring_portfolio.mvc.calculator;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/calculator")
+public class CalculatorAPIController {
+
+    @GetMapping("/{expression}")
+    public ResponseEntity<String> getResult(@PathVariable String expression) {
+
+        // Creating object calc from the Calculator class
+        Calculator calc = new Calculator(expression);
+        // Calls toString
+        String result = calc.toString();
+        if (result != null) {
+            return new ResponseEntity<String>(result, HttpStatus.OK);
+        }
+
+        // Bad ID
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+}
